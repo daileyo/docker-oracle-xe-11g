eace01277d4c0e1aae4f313795b40ac20438d3b0
daileyo <dale13@gmail.com>
2017-01-26 19:18:54 -0800

    Updated docker file to include volume. Modified startup.sh to rename files after being ran, so that they would not get re-executed.

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
 FROM ubuntu:16.04
 
-MAINTAINER Wei-Ming Wu <wnameless@gmail.com>
+MAINTAINER Daileyo (dale13@gmail.com)
 
 ADD assets /assets
 RUN /assets/setup.sh
 
-EXPOSE 22
+ENV ORACLE_HOME /u01/app/oracle/product/11.2.0/xe 
+ENV PATH $ORACLE_HOME/bin:$PATH 
+ENV ORACLE_SID=XE
+
 EXPOSE 1521
 EXPOSE 8080
+
+VOLUME ["/u01/app/oracle"]  
 
 CMD /usr/sbin/startup.sh && /usr/sbin/sshd -D
--- assets/startup.sh
+++ assets/startup.sh
@@ -21,8 +21,14 @@ fi
 
 for f in /docker-entrypoint-initdb.d/*; do
   case "$f" in
-    *.sh)     echo "$0: running $f"; . "$f" ;;
-    *.sql)    echo "$0: running $f"; echo "exit" | /u01/app/oracle/product/11.2.0/xe/bin/sqlplus "SYSTEM/oracle" @"$f"; echo ;;
+    *.sh)     	echo "$0: running $f"; . "$f" ;;
+    *.sql)	echo "$0: running $f"
+	      	echo "exit" | /u01/app/oracle/product/11.2.0/xe/bin/sqlplus "SYS/oracle as sysdba" @"$f"
+	      	echo
+		echo "Setting to ran."
+		mv -- "$f" "${f%.sql}.sql.ran"
+		echo
+		;;
     *)        echo "$0: ignoring $f" ;;
   esac
   echo
